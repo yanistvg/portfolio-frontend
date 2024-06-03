4f459ed19466014ef9c69c0dcff468848f403de3
Yanis GENY <yanisgenytv@gmail.com>
2024-06-03 11:44:20 +0200

    add navigation bar maybe change design in future

--- src/app/Navigator/Navigator.tsx
+++ src/app/Navigator/Navigator.tsx
@@ -1,6 +1,76 @@
+import { Flex, Text, View } from "@aws-amplify/ui-react";
+import { navigationBtn, NAV_MAX_WIDTH } from "./navigatiorDatas";
+import { FaAngleDoubleLeft } from "react-icons/fa";
+import { useEffect, useState } from "react";
+
+import "./Navigator.scss";
 
 export function Navigator(): JSX.Element {
+    const [ navOpened, setNavOpened ] = useState<boolean>(false);
+
+    useEffect(() => {
+        let navInfo: string | null = localStorage.getItem('navigationState');
+
+        if (!navInfo) {
+            localStorage.setItem('navigationState', 'false');
+            navInfo = 'false';
+        }
+
+        return navInfo === 'true'
+            ? setNavOpened(true)
+            : setNavOpened(false)
+        ;
+    }, []);
+
     return (
-        <>Bien le bonjour</>
+        <View
+            width={NAV_MAX_WIDTH}
+            id={'navigator-racine'}
+            className={!navOpened ? "navigation-closed" : ""}
+        >
+
+            <View className={"navigation-close-navBar"}>
+                <FaAngleDoubleLeft
+                    height={2}
+                    onClick={() => {
+                        localStorage.setItem('navigationState', (!navOpened).toString());
+                        setNavOpened(!navOpened);
+                    }}
+                />
+            </View>
+
+            {navigationBtn.map((navGroup, i) => {
+                return (
+                    <Flex
+                        direction={'column'}
+                        key={i}
+                    >
+                        <Text className={"navigation-title"}>{navGroup.name}</Text>
+                        {navGroup.nav.map((nav, y) => {
+                            return (
+                                <Flex
+                                    key={y}
+                                    className={"navigation-button"}
+                                    onClick={() => {
+                                        if (nav.isExternalLink) {
+                                            window.open(nav.link);
+                                            return;
+                                        }
+                                        window.history.replaceState(null, nav.name, nav.link);
+                                        document.title = nav.name;
+                                    }}
+                                >
+                                    <Flex>
+                                        { nav.icon }
+                                        <Text>{nav.name}</Text>
+                                    </Flex>
+                                </Flex>
+                            );
+                        })}
+                    </Flex>
+                );
+            })}
+
+        </View>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 
 import { Navigator } from './app';
+
+import '@aws-amplify/ui-react/styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
